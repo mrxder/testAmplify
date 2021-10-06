44a73b09a16e751649397cf79109fb0ed69d670a
Maximilian Torggler <inquiries@maximilian-torggler.dev>
2021-10-06 10:59:22 +0200

    feat: i19n test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
-export default function Home() {
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+
+export const getStaticProps: GetStaticProps<{ greeting: string }> = async ({
+  locale: localeP,
+}) => {
+  return {
+    props: {
+      greeting: `(${localeP}) 127 Straßengang, with da cool kids:`,
+    },
+  };
+};
+
+export default function Home({
+  greeting,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
       <main>
         <h1 className="font-mono text-xl code">
-          Welcome to <span className="text-purple-700">Nextjs</span>, <span className="text-indigo-700">TailwindCSS</span> and <span className="text-gray-700">TypeScript</span>
+          {greeting} <span className="text-purple-700">Nextjs</span>,{" "}
+          <span className="text-indigo-700">TailwindCSS</span> and{" "}
+          <span className="text-gray-700">TypeScript</span>
         </h1>
       </main>
     </div>
-  )
+  );
 }
